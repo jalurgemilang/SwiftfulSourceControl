14410afa8fa01af8a348c14de5a72e7efe1b8010
FY Long <longfy@ringgit.me>
2024-05-18 17:16:53 +0800

    [Clean] Reorder Commit Message Documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -11,17 +11,19 @@
  NEW FEATURE:
  [Feature] Description of the feature
  
- BUG IN PRODUCTION:
- [Patch] Description of the patch
- 
  BUG NOT IN PRODUCTION:
  [Bug] Description of the bug
+ 
+ RELEASE:
+ [Release[ Description of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
  
  MUNDANE TASKS:
  [Clean] Description of changes
  
- RELEASE:
- [Release[ Description of release
+ 
  
  
  
