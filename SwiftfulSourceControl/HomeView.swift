f53fb8d5e178b08ec38781ec68330d9d8e89258d
FY Long <longfy@ringgit.me>
2024-05-18 21:03:17 +0800

    Typo error for var title

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var totle: String = ""
+    @State private var title: String = ""
     var body: some View {
         ZStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
