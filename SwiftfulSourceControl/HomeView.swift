079ceb4e0b617ba4e1faa0b1277be57edbdcfe8c
FY Long <longfy@ringgit.me>
2024-05-18 16:44:42 +0800

    Add Home View

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Long Fong Yee on 18/05/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Yo")
+    }
+}
+
+#Preview {
+    HomeView()
+}
