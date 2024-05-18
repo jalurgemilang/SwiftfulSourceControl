8df7a60029ca576018320d422e7117080c39af29
FY Long <longfy@ringgit.me>
2024-05-18 16:54:38 +0800

    [Clean] Add commit messages

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,28 @@
 //  Created by Long Fong Yee on 18/05/2024.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release[ Description of release
+ 
+ 
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
