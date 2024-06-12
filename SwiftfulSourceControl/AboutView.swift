d10ec0d7d168a0394599dcf9af95110e7e601ccf
jalurgemilang <80514870+jalurgemilang@users.noreply.github.com>
2024-06-12 11:22:18 +0800

    [New] About page

---
+++ SwiftfulSourceControl/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Long Fong Yee on 12/06/2024.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("About!")
+    }
+}
+
+#Preview {
+    AboutView()
+}
