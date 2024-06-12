0acea2d0ffd78efae2de85ba53d252873114c25f
jalurgemilang <80514870+jalurgemilang@users.noreply.github.com>
2024-06-12 10:59:06 +0800

    [Added] New file called ExploreView

---
+++ SwiftfulSourceControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Long Fong Yee on 12/06/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore View!")
+    }
+}
+
+#Preview {
+    ExploreView()
+}
