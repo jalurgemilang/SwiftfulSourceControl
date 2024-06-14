c56e64d348eb01b467993b53535e61bd9429881d
MacBook Air <macbookair@ringgit.me>
2024-06-14 09:57:47 +0800

    [Add] Lookup Table

---
+++ SwiftfulSourceControl/CoLabsTableView.swift
@@ -0,0 +1,20 @@
+//
+//  CoLabsTableView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Long Fong Yee on 14/06/2024.
+//
+
+import SwiftUI
+
+struct CoLabsTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//Lookup table
+
+#Preview {
+    CoLabsTableView()
+}
